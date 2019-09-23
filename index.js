1ebaa161c41ce53e9c4cb61f54d244a4ae609b52
burceje_sd2023@ceaccs.net <burceje_sd2023@ceaccs.net>
2019-09-23 16:25:57 +0800

    final

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express()
 var fs = require('fs')
 app.set('view engine', 'pug');
 app.set('views', 'view');
+var path = require('path');
+app.use(express.static(path.join(__dirname,'/public')));
 // respond the index.pug when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.render('index');
@@ -12,13 +14,15 @@ app.get('/provinces/:name', function (req, res) {
     console.log(provinceName) //pathname like bohol.json
 
     fs.readFile("./provinces/"+provinceName, function(err, data) {    
-        res.render('index', JSON.parse(data))//display data in the index
-        var alldata= JSON.parse(data);
-        console.log(alldata.data)
+        console.log(data);
+        console.log(JSON.parse(data));
+         var alldata= JSON.parse(data);
+        res.render('index',{image1:alldata.images[0],image2:alldata.images[1],image3:alldata.images[2], alldata: alldata } )//display data in the index
+        
         if(err){
         res.sendStatus(404);
         }
     
       });
 })
-app.listen(8081);
+app.listen(8000);
