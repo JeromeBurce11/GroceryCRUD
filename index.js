28505956b812cf02253e38b24c5fd0fd60fc9c53
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-10-07 17:15:23 +0800

    modified 5:15 10/7/2019

--- index.js
+++ index.js
@@ -7,56 +7,27 @@ var bodyParser = require('body-parser');
 var Item = require('./item')
 
 var url = "mongodb://localhost:27017/shop";
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true);
+mongoose.connect(url, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 
 app.put('/item/create', function (req, res) {
-        let test =  new Item(req.body)
-       
-        test.save(function(err,data){
-            if(err){
-                console.log('err')
-            }else{
-                console.log('saved')
-            }
-            Item.find(req.body ,function(err,item){
-                if(err){
-                    console.log('err')
-                }else{
-                   console.log(item);
-                   res.json(item)
-                }
-    
-            })
+    let test = new Item(req.body)
 
-        })
-})
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/view/index.html");
-})
-
-
-
-app.get('/item/retrieve/all', function (req, res) {
-    
-    let test =  new Item(req.body)
-       
-    test.save(function(err,data){
-        if(err){
+    test.save(function (err, data) {
+        if (err) {
             console.log('err')
-        }else{
+        } else {
             console.log('saved')
         }
-        Item.find(req.body ,function(err,item){
-            if(err){
+        Item.find(req.body, function (err, item) {
+            if (err) {
                 console.log('err')
-            }else{
-               console.log(item);
-               res.json(item)
+            } else {
+                res.json(item)
             }
 
         })
@@ -64,13 +35,80 @@ app.get('/item/retrieve/all', function (req, res) {
     })
 })
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/view/index.html");
+})
 
-app.post('/item/update/', function (req, res) {
-    res.send("File updated!")
+app.get('/item/retrieve/all', function (req, res) {
+    Item.find(req.body, function (err, item) {
+        if (err) {
+            console.log('err')
+        } else {
+            res.json(item)
+        }
+
+    })
+
+
+})
+
+app.get('/item/retrieve/:id', function (req, res) {
+    Item.findById(req.params.id)
+    .then(items => {
+        if(!items) {
+            return res.status(404).send({
+                message: "items not found with id " + req.params.id
+            });            
+        }
+        res.send(items);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "items not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving items with id " + req.params.id
+        });
+    });
+
+
+})
+
+app.put('/item/update/:id', function (req, res) {
+    Item.findOneAndUpdate({_id: req.body.id}, {item: req.body.item, Quantity: req.body.Quantity, Priority: req.body.Priority }, {
+        new: true
+      })
+
+    .then(items => {
+        if(!items) {
+            return res.status(404).send({
+                message: "items not found with id " + req.params.id
+            });            
+        }
+        res.json(items);
+        console.log(items)
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "items not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving items with id " + req.params.id
+        });
+    });
 })
 
 app.delete('/item/delete', function (req, res) {
-    res.send('Item delete!')
+    console.log(req.body);
+    Item.findOneAndDelete({_id: req.body.id}, function(err, data) {
+        if(err) return console.log(err);     
+        const response = {
+            message: "Successfully deleted",
+        };
+        return res.status(200).send(response);
+    })  
 })
 
 http.listen(port, function () {
